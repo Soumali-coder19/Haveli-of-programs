725a9dbccd9194f770ccf4b6679d611a7a9628a6
Soumali-coder19 <55548446+Soumali-coder19@users.noreply.github.com>
2019-10-16 16:12:58 +0530

    Sparse matrix implemention on mxn matrix
    A sparse matrix is a one in which the majority of the values are zero. The proportion of zero elements to non-zero elements is referred to as the sparsity of the matrix. The opposite of a sparse matrix, in which the majority of its values are non-zero, is called a dense matrix.
    Sparse matrices are used by scientists and engineers when solving partial differential equations. For example, a measurement of a matrix's sparsity can be useful when developing theories about the connectivity of computer networks. When using large sparse matrices in a computer program, it is important to optimize the data structures and algorithms to take advantage of the fact that most of the values will be zero.

---
+++ sparsemat.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int A[10][10],B[10][3],m,n,s=0,i,j;
+ //clrscr();
+ printf("\nEnter the order m x n of the sparse matrix:");
+ scanf("%d%d",&m,&n);
+ printf("\nEnter the elements in the sparse matrix(mostly zeroes):");
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   printf("\n%d row and %d column:   ",i,j);
+   scanf("%d",&A[i][j]);
+  }
+ }
+ printf("\nThe given matrix is:\n");
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   printf("%d ",A[i][j]);
+  }
+  printf("\n");
+ }
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   if(A[i][j]!=0)
+   {
+    B[s][0]=A[i][j];
+    B[s][1]=i;
+    B[s][2]=j;
+    s++;
+   }
+  }
+ }
+ printf("\nThe sparse matrix is given by:");
+ printf("\n");
+ for(i=0;i<s;i++)
+ {
+  for(j=0;j<3;j++)
+  {
+   printf("%d ",B[i][j]);
+  }
+  printf("\n");
+ }
+ getch();
+}
+
