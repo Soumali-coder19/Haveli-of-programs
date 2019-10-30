452e514804af65fb735fc68ba433387f1a59a718
Soumali-coder19 <55548446+Soumali-coder19@users.noreply.github.com>
2019-10-30 15:36:59 +0530

    Add files via upload
    in a matrix, where suppose n is the user input, we obtain an 2n-1x2n-1 matrix where n is displayed in outer boundary and decrements by 1 as the boundary approaches center.

---
+++ rectangle decrement mat.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i,j,A[500][500],num,temp;
+	printf("\nEnter a number below 50(pawsitive) plixx : "); //thats all u guna do u madafaka user
+	scanf("%d",&num);
+	temp=2*num-1;
+	for(i=0;i<temp;i++)
+	{
+		for(j=i;j<=temp-1-i;j++)           //condition for j for 1st row
+		{
+			A[i][j]=num;
+		}
+		for(j=i+1;j<=temp-1-i;j++)       //condition for j for n-1th column
+		{
+			A[j][temp-i-1]=num;
+		}
+		for(j=temp-i-2;j>=i;j--)   //condition for j for n-1th row
+		{
+			A[temp-i-1][j]=num;
+		}
+		for(j=temp-i-2;j>i;j--)    //condition for j for 1st column
+		{
+			A[j][i]=num;
+		}
+		num--;
+	}
+	
+	for(i=0;i<temp;i++)  //print array
+	{
+		for(j=0;j<temp;j++)
+		{
+			printf("%d\t",A[i][j]);  //2 digit dile value thik asche but disrupt hoye jachhe amar love life er moto
+		}
+		printf("\n");        //
+	}
+	return 0;
+	
+}
